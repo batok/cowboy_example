3950ea9f66cb9e79707875dd8f15e91d1749ef23
Domingo Aguilera <domingo.aguilera@gmail.com>
2020-06-12 16:41:25 +0000

    add logging of ranch:info/1 when executing a request

--- lib/cowboy_example.ex
+++ lib/cowboy_example.ex
@@ -2,6 +2,7 @@ defmodule CowboyExample do
   @moduledoc """
   Documentation for `CowboyExample`.
   """
+  require Logger
 
   @doc """
   Hello world.
@@ -18,6 +19,8 @@ defmodule CowboyExample do
 
   def init(req0, opts) do
     req = :cowboy_req.reply(200, %{"content-type" => "text/plain"}, "Hello world!", req0)
+    info = :ranch.info(:http)
+    Logger.info("ranch:info/1 when request : #{inspect info}")
     {:ok, req, opts}
   end
 end
